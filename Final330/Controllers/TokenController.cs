2cc188f55dee34fa35287d4ec8069c41167aa179
MatC <matcomtois@gmail.com>
2022-09-07 18:38:21 -0700

    Token Post/Get added to project and Postman

--- Final330/Controllers/TokenController.cs
+++ Final330/Controllers/TokenController.cs
@@ -1,33 +1,55 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Final330.Models;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace Final330.Controllers
 {
     public class TokenRequest
     {
-        public string UserName { get; set; }
+        [JsonPropertyName("email_address")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [JsonPropertyName("password")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 
-    [Route("api/[controller]")]
+    [Route("api/login")]
     [ApiController]
     public class TokenController : ControllerBase
     {
-        // This should require SSL
         [HttpPost]
         public dynamic Post([FromBody] TokenRequest tokenRequest)
         {
-            var token = TokenHelper.GetToken(tokenRequest.UserName, tokenRequest.Password);
+            var token = TokenHelper.GetToken(tokenRequest.EmailAddress, tokenRequest.Password);
             return new { Token = token };
         }
 
-        // This should require SSL
         [HttpGet]
-        [Route("{userName}/{password}")]
-        public dynamic Get(string userName, string password)
+        [Route("{email}/{password}")]
+        public dynamic Get(string email, string password)
         {
-            var token = TokenHelper.GetToken(userName, password);
+            var token = TokenHelper.GetToken(email, password);
             return new { Token = token };
         }
+
+        //tried creating tokens from UsersRepo, but I couldn't eliminate request body fields from the equation
+        //this resulted in SwaggerUI using the request body but requiring an id/user data
+        //[HttpPost("{id}")]
+        //public IActionResult Post(int id, TokenRequest tokenRequest)
+        //{
+        //    var user = userRepo.Users.FirstOrDefault(t => t.Id == id);
+        //    if (user == null)
+        //    {
+        //        return BadRequest("You suck.");
+        //    }
+        //    tokenRequest.EmailAddress = user.Email;
+        //    tokenRequest.Password = user.Password;
+        //    var token = TokenHelper.GetToken(tokenRequest.EmailAddress, tokenRequest.Password);
+        //    return Ok(token);
+        //}
     }
 }
